de4e047c115c0b76af5b8c502fb40f45342df309
kimmi <kimmi@DESKTOP-25ANRB9>
2022-05-20 01:15:03 +0900

    패키지 추가

---
+++ DatabaseTerm/src/main/java/com/teammate/find/Project/ProjectController.java
@@ -0,0 +1,10 @@
+package com.teammate.find.Project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProjectController {
+	
+}
